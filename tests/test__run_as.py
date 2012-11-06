81f00eb6405b31f0704a10ff1b4a0e136548688d
Guy Rozendorn <guy@rzn.co.il>
2012-11-06 13:11:23 +0200

    TRIVIAL implementation and code

---
+++ tests/test__run_as.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, SkipTest
+from mock import patch
+from os import name, environ
+
+class RunAsTestCase(TestCase):
+    def test__on_unix_with_mock(self):
+        if name == 'nt':
+            raise SkipTest("unix only")
+        with patch("infi.run_as.subprocess_runas_context"):
+            from infi.run_as import run_as
+            self.assertEquals(0, run_as(["Administrator", "Password", "ifconfig"]))
+
+    def test__on_windows(self):
+        if name != 'nt':
+            raise SkipTest("windows only")
+        username, password = environ['RUNAS_USERNAME'], environ['RUNAS_PASSWORD']
+        self.assertEquals(0, run_as([username, password, "ipconfig"]))
