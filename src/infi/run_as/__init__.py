31e58485de7f28f26c16259c0cbfaf8fdf8007c8
Guy Rozendorn <guy@rzn.co.il>
2014-05-18 17:47:40 +0300

    fixing the recursion in a different way

--- src/infi/run_as/__init__.py
+++ src/infi/run_as/__init__.py
@@ -9,26 +9,26 @@ from sys import argv, stderr, stdout, exit
 from .c_api import Environment, StartupInfoW, ProcessInformation, CreateProcessWithLogonW, WaitForInputIdle, Handle
 from .c_api import create_buffer, create_unicode_buffer, Ctypes, get_token, CreateProcessAsUserW, INFINITE
 
+
 logger = getLogger(__name__)
+
 
 class CreateProcess(object):
     def __init__(self, username, password):
         super(CreateProcess, self).__init__()
         self.username = username
         self.password = password
+        self.method = self._CreateProcessWithLogon if \
+                      self._can_we_user_create_process_with_login() else \
+                      self._CreateProcessAsUser
+
+    def _can_we_user_create_process_with_login(self):
+        return Windows().is_windows_2003() and environ.get('USERNAME', 'SYSTEM') == 'SYSTEM'
 
     def create_process_as_administrator(self, *args, **kwargs):
         """ A drop-in replacement for _subprocess.CreateProcess that creates the process as an administrator
         :returns: a 4-tuple (proc_handle, thread_handle, pid, tid)"""
-        if Windows().is_windows_2003() and environ.get('USERNAME', 'SYSTEM') == 'SYSTEM':
-            # http://msdn.microsoft.com/en-us/library/windows/desktop/ms682431(v=vs.85).aspx
-            # Windows XP with SP2 and Windows Server 2003:
-            # You cannot call CreateProcessWithLogonW from a process that is running under the "LocalSystem" account,
-            # because the function uses the logon SID in the caller token,
-            # and the token for the "LocalSystem" account does not contain this SID.
-            # As an alternative, use the CreateProcessAsUser and LogonUser functions.
-            return self._CreateProcessAsUser(*args, **kwargs)
-        return self._CreateProcessWithLogon(*args, **kwargs)
+        return self.method(*args, **kwargs)
 
     def _CreateProcessWithLogon(self, app_name, cmd_line, proc_attrs, thread_attrs, inherit,
                                 flags, env_mapping, curdir, startup_info):
@@ -87,11 +87,13 @@ class CreateProcess(object):
                   processInformation_.dwProcessId, processInformation_.dwThreadId)
         return result
 
+
 @contextmanager
 def subprocess_runas_context(username, password):
-    with patch("_subprocess.CreateProcess") as CreateProcess_:
-        CreateProcess_.side_effect = CreateProcess(username, password).create_process_as_administrator
+    side_effect = CreateProcess(username, password).create_process_as_administrator
+    with patch("_subprocess.CreateProcess", new=side_effect):
         yield
+
 
 def run_as(argv=argv[1:]):
     from infi.execute import execute
