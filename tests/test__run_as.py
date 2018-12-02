1bcd5e95fa1558621992d998313193c2bf8d591a
Arnon Yaari <arnony@infinidat.com>
2018-12-02 14:36:13 +0200

    Python 3 support

--- tests/test__run_as.py
+++ tests/test__run_as.py
@@ -8,11 +8,11 @@ class RunAsTestCase(TestCase):
             raise SkipTest("unix only")
         with patch("infi.run_as.subprocess_runas_context"):
             from infi.run_as import run_as
-            self.assertEquals(0, run_as(["Administrator", "Password", "ifconfig"]))
+            self.assertEqual(0, run_as(["Administrator", "Password", "ifconfig"]))
 
     def test__on_windows(self):
         from infi.run_as import run_as
         if name != 'nt':
             raise SkipTest("windows only")
         username, password = environ['RUNAS_USERNAME'], environ['RUNAS_PASSWORD']
-        self.assertEquals(0, run_as([username, password, "ipconfig"]))
+        self.assertEqual(0, run_as([username, password, "ipconfig"]))
